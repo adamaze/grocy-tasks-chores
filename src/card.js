63d33a8a63130e11ee3a2dfa96ec832b0b7bf48c
Adam Maryniuk <adamaze@gmail.com>
2025-02-01 09:02:39 -0600

    dark-shot-dark

--- src/card.js
+++ src/card.js
@@ -36,7 +36,6 @@ export class GrocyTasksChoresCard extends LitElement {
     _dueDays;
     _texts;
     _userId;
-    _categoryId;
     _hideNotDue;
     _hideAlmostDue;
     _hideDue;
@@ -126,7 +125,6 @@ export class GrocyTasksChoresCard extends LitElement {
         this._almostDueDays = config.almostDueDays ?? 3;
         this._dueDays = config.dueDays ?? 0;
         this._userId = config.userId ?? 0;
-        this._categoryId = config.categoryId ?? 0;
         this._hideNotDue = config.hideNotDue ?? false;
         this._hideAlmostDue = config.hideAlmostDue ?? false;
         this._hideDue = config.hideDue ?? false;
@@ -379,7 +377,7 @@ export class GrocyTasksChoresCard extends LitElement {
             || this._hideAlmostDue && dueClass === 'almostDue'
             || this._hideDue && dueClass === 'due'
             || this._filterUsers && this._filterUsers.indexOf(userId) === -1
-            || this._filterCategories && this._filterCategories.indexOf(categoryId) === -1
+            || this._filterCategories && this._filterCategories.indexOf(userId) === -1
     }
 
     _processUser(user) {
